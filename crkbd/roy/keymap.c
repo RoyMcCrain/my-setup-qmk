f2e1bb93c296ed63ce5601934eaddc8125202910
RoyMcCrain <matsuda.r@kanamekey.com>
2020-01-20 14:54:23 +0900

    Add Tab

--- crkbd/roy/keymap.c
+++ crkbd/roy/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, KC_UNDO,  KC_CUT, KC_COPY, KC_PSTE, KC_LBRC,                      KC_RBRC, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_DEL,  LOWER2,  KC_SPC,     KC_ENT,  RAISE1,  CTRLE \
+                                           KC_DEL,  LOWER2,  KC_TAB,     KC_ENT,  RAISE1,  CTRLE \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RALT \
+                                          KC_LGUI,  LOWER2,  KC_SPC,     KC_ENT,  RAISE1, KC_RALT \
                                       //`--------------------------'  `--------------------------'
   )
 };
